bdb3169533b389495fbb138b6de337b4a1bb0e1c
namanjain123 <naman.nishi@gmail.com>
2024-01-11 19:09:43 +0530

    fixed emmbedding code

--- Semantic-Kernel-RAG/Services/Service/LoadMemoryService.cs
+++ Semantic-Kernel-RAG/Services/Service/LoadMemoryService.cs
@@ -4,13 +4,15 @@ using Microsoft.Extensions.Logging;
 using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.Connectors.Qdrant;
 using Microsoft.SemanticKernel.Connectors.HuggingFace;
+using Microsoft.SemanticKernel.Memory;
+using Azure.Core;
 namespace Services;
 
 public class LoadMemoryService : ILoadMemoryService
 {
     private readonly IConfiguration _config;
     private readonly ILogger<LoadMemoryService> _logger;
-
+    private SemanticTextMemory textMemory;
     public LoadMemoryService(IConfiguration config, ILogger<LoadMemoryService> logger)
     {
         _config = config;
@@ -24,26 +26,23 @@ public class LoadMemoryService : ILoadMemoryService
             _logger.LogError("No text files provided. Use '--help' for usage.");
             return "No File Found";
         }
-#pragma warning disable SKEXP0020 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
-        HuggingFaceTextEmbeddingGenerationService embeddiingService = new HuggingFaceTextEmbeddingGenerationService("BAAI/bge-large-en-v1.5", "http://0.0.0.0:8080");
-#pragma warning restore SKEXP0020 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
-        string memoryStringConnection = _config["Quadrant:memory"] ?? "";
+        HuggingFaceTextEmbeddingGenerationService embeddiingService = new HuggingFaceTextEmbeddingGenerationService(_config["Embedding:ModelName"], _config["Embedding:Endopint"]);
+        string memoryStringConnection = _config["Quadrant:memoryUrl"] ?? "";
         if (string.IsNullOrWhiteSpace(memoryStringConnection))
         {
             _logger.LogError("Please set the connection string of the memory");
             return "Keys not Found";
         }
-#pragma warning disable SKEXP0026 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
-        var memoryStore = new QdrantMemoryStore(memoryStringConnection, 1536);
-#pragma warning restore SKEXP0026 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+        int vectorSize = int.Parse(_config["Quadrant:vectorSize"] ?? "0");
+        var memoryStore = new QdrantMemoryStore(memoryStringConnection, vectorSize);
+        //Store Kernel
+        textMemory = new (memoryStore,embeddiingService);
+        await ImportMemoriesAsync(textMemory, collection, textFile);
 
-        await ImportMemoriesAsync(embeddiingService, collection, textFile);
         return "Import Done";
     }
-    private async Task ImportMemoriesAsync(dynamic kernel, string collection, params FileInfo[] textFile)
+    private async Task ImportMemoriesAsync(SemanticTextMemory kernel, string collection, params FileInfo[] textFile)
     {
-        // Use sequential memory IDs; this makes it easier to retrieve sentences near a given sentence.
-        int memoryId = 0;
         // Import the text files.
         int fileCount = 0;
         //Load Into the Memory
@@ -52,7 +51,9 @@ public class LoadMemoryService : ILoadMemoryService
             // Read the text file.
             string text = File.ReadAllText(fileInfo.FullName);
             // Split the text into sentences.
+            // Split the text into sentences.
             string[] sentences = BlingFireUtils.GetSentences(text).ToArray();
+
             // Save each sentence to the memory store.
             int sentenceCount = 0;
             foreach (string sentence in sentences)
@@ -61,15 +62,18 @@ public class LoadMemoryService : ILoadMemoryService
                 if (sentenceCount % 10 == 0)
                 {
                     // Log progress every 10 sentences.
+                    _logger.LogInformation($"[{fileCount}/{fileInfo.Length}] {fileInfo.FullName}: {sentenceCount}/{sentences.Length}");
                 }
 
-                await kernel.Memory.SaveInformationAsync(
-                    collection: collection,
-                    text: sentence,
-                    id: memoryId++.ToString(),
-                    description: sentence);
+                try
+                {
+                    string id = Guid.NewGuid().ToString();
+                    var x = await kernel.SaveInformationAsync(collection, id: id, text: sentence);
+                }
+                catch { }
             }
-        }
+
+    }
     }
 
 
